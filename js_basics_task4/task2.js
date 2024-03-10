41af6600c4f1c3c5bc37ced63c74c7e9d9fdbdeb
Vladimir Gazevych <vladimir.gazevych@gmail.com>
2024-03-10 03:18:59 +0200

    HW5

---
+++ js_basics_task4/task2.js
@@ -0,0 +1,30 @@
+// Є масив рядків
+
+// const strings = ["String one", "String two", "string three"]
+// За допомогою циклу for of пройтись по всім елементам та вивести у консоль всі рядки великими буквами. Приклад:
+
+// STRING ONE
+// STRING TWO
+// STRING THREE
+
+const strings = ["String one", "String two", "string three"]
+const string1 = []
+const string2 = []
+const string3 = []
+
+for(const string of strings) {
+    string1.push(string.toUpperCase())
+}
+console.log(string1[string1.length-3]);
+
+
+for(const string of strings) {
+    string1.push(string.toUpperCase())
+}
+console.log(string1[string1.length-2]);
+
+
+for(const string of strings) {
+    string1.push(string.toUpperCase())
+}
+console.log(string1[string1.length-1]);
