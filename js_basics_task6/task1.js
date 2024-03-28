76fb140c83f3c920083bce120b31cdb3080e5bfd
Vladimir Gazevych <vladimir.gazevych@gmail.com>
2024-03-28 18:27:23 +0200

    HW6

---
+++ js_basics_task6/task1.js
@@ -0,0 +1,20 @@
+// Використовуючи рекурсію, напишіть функцію, яка приймає два параметри - start та finish. За допомогою рекурсії, виведіть всі числа від start до finish.
+
+// Приклад використання:
+
+// printIntervalRec(10,15);
+// Результат:
+// 10
+// 11
+// 12
+// 13
+// 14
+// 15
+
+function countToFift(startValue) {
+    for (let = startValue; i <=15; i++) {
+        console.log(i);
+    }
+}
+
+countToFift(10)
