4d262ddd5351393ab1cbc129e1493d888d68831f
Vladimir Gazevych <vladimir.gazevych@gmail.com>
2024-04-05 19:10:41 +0300

    HW8

--- js_basics_task8/task1.js
+++ js_basics_task8/task1.js
@@ -5,43 +5,59 @@
 // Створити дочірній клас Cat від Animal та в ньому додати 3 індивідуальні властивості для котів. І 2 індивідуальні методи.
 // Створити об'єкти до Dog та Cat та попрактикуватись із методами.
 
-let Animal = {
-    type: "animal",
-    color: "color",
-    name: "name",
-    speed: "speed",
-    sayHello: function () {
-        console.log(`The animal name is ${this.name}`),
-        console.log(`The color is ${this.color}`),
-        console.log(`The type of animal: ${this.type}`)
-    },
-    run: function () {
-        console.log(`The animal speed is ${this.speed}`)
+class Animals {
+    constructor(animal, color, name, speed, harchi) {
+    this.animal = animal;
+    this.color = color;
+    this.name = name;
+    this.speed = speed;
+    this.harchi = harchi;
+}
+    sayHello() {
+        console.log(`The animal name is ${this.name}`);
+        console.log(`The color is ${this.color}`);
+        console.log(`The type of animal: ${this.animal}`);
+    };
+    run() {
+        console.log(`The animal speed is ${this.speed}`);
+    };
+    eat() {
+        console.log(`The animal eat is ${this.harchi}`);
+        
     }
 }
 
-let animalCat = Object.create(Animal);
-animalCat.name = "Barsik";
-animalCat.type = "Cat";
-animalCat.color = "Black";
-animalCat.years = 2020;
-animalCat.poroda = "Sfinks";
-animalCat.speed = "15 km\h",
+const animalCat = new Animals ("Cat", "Grey", "Barsik", "15 km\h", "Milk");
+const animalDog = new Animals ("Dog", "Black", "Sirko", "25 km\h", "Meat");
 
-console.log(animalCat);
 animalCat.sayHello();
 animalCat.run();
-console.log("---------------")
-
-let animalDog = Object.create(Animal);
-animalDog.name = "Sirko";
-animalDog.type = "Dog";
-animalDog.color = "Grey";
-animalDog.years = 2021;
-animalDog.sexo = "suka";
-animalDog.hunter = "Yes";
-animalDog.speed = "35 km\h";
-
-console.log(animalDog);
+animalCat.eat();
+console.log("-------------")
 animalDog.sayHello();
 animalDog.run();
+animalDog.eat();
+// animalCat.name = "Barsik";
+// animalCat.type = "Cat";
+// animalCat.color = "Black";
+// animalCat.years = 2020;
+// animalCat.poroda = "Sfinks";
+// animalCat.speed = "15 km\h",
+
+// console.log(animalCat);
+// animalCat.sayHello();
+// animalCat.run();
+// console.log("---------------")
+
+// const animalDog = Object.create(Animals);
+// animalDog.name = "Sirko";
+// animalDog.type = "Dog";
+// animalDog.color = "Grey";
+// animalDog.years = 2021;
+// animalDog.sexo = "suka";
+// animalDog.hunter = "Yes";
+// animalDog.speed = "35 km\h";
+
+// console.log(animalDog);
+// animalDog.sayHello();
+// animalDog.run();
